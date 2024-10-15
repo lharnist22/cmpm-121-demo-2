0d1286e0bf39e68160da73843a92635ba692e890
lharnist <lharnist@ucsc.edu>
2024-10-15 13:10:50 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,11 @@ const APP_NAME = "Luc's Game!";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+const titleElement = document.createElement("h1");
+titleElement.textContent = APP_NAME;
+app.appendChild(titleElement);
+
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+app.appendChild(canvas);
